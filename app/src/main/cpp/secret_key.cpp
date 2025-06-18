dc8ecd5334a0d655442550686f3e7d40ec709717
Md Taiful Islam <tomal2514@gmail.com>
2025-06-18 22:02:56 +0600

    Remove signature check method from secret_key.cpp file

--- app/src/main/cpp/secret_key.cpp
+++ app/src/main/cpp/secret_key.cpp
@@ -117,68 +117,6 @@ bool isRooted() {
     return false; // No root indicators found
 }
 
-// Signature check
-bool isSignatureValid(JNIEnv* env, jobject context) {
-    jclass contextClass = env->GetObjectClass(context);
-
-    jmethodID getPackageManager = env->GetMethodID(contextClass, "getPackageManager", "()Landroid/content/pm/PackageManager;");
-    jobject pm = env->CallObjectMethod(context, getPackageManager);
-
-    jmethodID getPackageName = env->GetMethodID(contextClass, "getPackageName", "()Ljava/lang/String;");
-    jstring packageName = (jstring)env->CallObjectMethod(context, getPackageName);
-
-    jclass pmClass = env->GetObjectClass(pm);
-    jmethodID getPackageInfo = env->GetMethodID(pmClass, "getPackageInfo",
-                                                "(Ljava/lang/String;I)Landroid/content/pm/PackageInfo;");
-    jobject pkgInfo = env->CallObjectMethod(pm, getPackageInfo, packageName, 0x08000000); // GET_SIGNING_CERTIFICATES
-
-    jclass pkgInfoClass = env->GetObjectClass(pkgInfo);
-    jfieldID signingInfoField = env->GetFieldID(pkgInfoClass, "signingInfo", "Landroid/content/pm/SigningInfo;");
-    jobject signingInfo = env->GetObjectField(pkgInfo, signingInfoField);
-
-    jclass signingInfoClass = env->GetObjectClass(signingInfo);
-    jmethodID getSigners = env->GetMethodID(signingInfoClass, "getApkContentsSigners", "()[Landroid/content/pm/Signature;");
-    jobjectArray signatures = (jobjectArray)env->CallObjectMethod(signingInfo, getSigners);
-
-    jobject signature = env->GetObjectArrayElement(signatures, 0);
-    jclass signatureClass = env->GetObjectClass(signature);
-    jmethodID toByteArray = env->GetMethodID(signatureClass, "toByteArray", "()[B");
-    jbyteArray sigBytes = (jbyteArray)env->CallObjectMethod(signature, toByteArray);
-
-    // Now hash the signature
-    jclass messageDigestClass = env->FindClass("java/security/MessageDigest");
-    jmethodID getInstance = env->GetStaticMethodID(messageDigestClass, "getInstance", "(Ljava/lang/String;)Ljava/security/MessageDigest;");
-    jstring algo = env->NewStringUTF("SHA-256");
-    jobject digest = env->CallStaticObjectMethod(messageDigestClass, getInstance, algo);
-
-    jmethodID update = env->GetMethodID(messageDigestClass, "update", "([B)V");
-    env->CallVoidMethod(digest, update, sigBytes);
-
-    jmethodID digestMethod = env->GetMethodID(messageDigestClass, "digest", "()[B");
-    jbyteArray hash = (jbyteArray)env->CallObjectMethod(digest, digestMethod);
-
-    // Convert hash to hex string
-    jsize len = env->GetArrayLength(hash);
-    jbyte* hashBytes = env->GetByteArrayElements(hash, NULL);
-
-    char hex[3 * 32]; // 32 bytes max, 2 chars per byte + ':'s
-    char* ptr = hex;
-
-    for (int i = 0; i < len; ++i) {
-        sprintf(ptr, "%02X", (unsigned char)hashBytes[i]);
-        ptr += 2;
-        if (i != len - 1) *ptr++ = ':';
-    }
-    *ptr = '\0';
-
-    env->ReleaseByteArrayElements(hash, hashBytes, JNI_ABORT);
-
-    std::string calculated(hex);
-    std::string expected = "71:02:D2:0E:28:53:2B:96:B9:74:1D:1E:FA:D0:21:F9:A8:A2:01:C2:C6:1E:C9:F4:77:8D:63:DF:36:C1:34:DA";
-
-    return calculated == expected;
-}
-
 // Emulator detection
 bool isProbablyEmulator() {
     char prop[PROP_VALUE_MAX];
@@ -206,25 +144,15 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
     }
     applyAntiDebugging(); // Call early
 
-    // Grab context via Java class (must pass from app)
-    jclass activityThread = env->FindClass("android/app/ActivityThread");
-    jmethodID currentApplication = env->GetStaticMethodID(activityThread, "currentApplication", "()Landroid/app/Application;");
-    jobject context = env->CallStaticObjectMethod(activityThread, currentApplication);
-
-
-/*
-    if (isRooted() || !isSignatureValid(env, context)) {
-        LOGE("Security check failed. Exiting...");
+    if (isRooted()) {
+        LOGE("Security check failed Device rooted. Exiting...");
         exit(1); // kill app
     }
-*/
 
-/*
     if(isProbablyEmulator()){
         LOGE("Security check failed Emulator. Exiting...");
         exit(1); // kill app
     }
-*/
 
 
     LOGI("All security checks passed.");
